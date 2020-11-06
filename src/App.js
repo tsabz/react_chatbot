fb481dc31fbc94efcc4940bf011fe21e259c0dc3
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-11-05 19:35:36 -0500

    chat box now delivers hello world

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Chatbot from 'react-chatbot-kit';
 
-import Config from './chatbot/Config.js';
-import ActionProvider from './chatbot/ActionProvider.js';
-import MessageParser from './chatbot/MessageParser.js';
+import config from './chatbot/config';
+import ActionProvider from './chatbot/ActionProvider';
+import MessageParser from './chatbot/MessageParser';
 
 import './App.css';
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <div style={{maxWidth: "300px"}}>
-        <Chatbot />
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
       </div>
     </div>
   );
--- src/chatbot/Config.js
+++ src/chatbot/Config.js
@@ -1,7 +1,7 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
+import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-    intialMessages: [createChatBotMessage(`Hello world`)],
-}
+    initialMessages: [createChatBotMessage('Hello world')],
+};
 
 export default config;
